4b20efe8613c880124e543c2a346f176feda6e51
MillerSenior <anthony.millersr82@gmail.com>
2025-05-29 18:38:03 -0500

    change pwd done

--- src/main/java/com/darro_tech/revengproject/controllers/account/AccountController.java
+++ src/main/java/com/darro_tech/revengproject/controllers/account/AccountController.java
@@ -7,6 +7,7 @@ import com.darro_tech.revengproject.repositories.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -36,46 +41,73 @@ public class AccountController extends BaseController {
     }
 
     @PostMapping("/change-password")
-    public String processChangePassword(
+    public Object processChangePassword(
             @RequestParam String currentPassword,
             @RequestParam String newPassword,
             @RequestParam String confirmPassword,
             HttpSession session,
             RedirectAttributes redirectAttributes,
-            Model model) {
+            Model model,
+            HttpServletRequest request) {
 
         logger.info("Processing password change request");
+        boolean isAjaxRequest = "XMLHttpRequest".equals(request.getHeader("X-Requested-With"));
+        Map<String, Object> response = new HashMap<>();
 
         // Get the current user
         User user = authenticationController.getUserFromSession(session);
         if (user == null) {
             logger.warn("No user found in session when attempting to change password");
-            redirectAttributes.addFlashAttribute("error", "You must be logged in to change your password");
-            return "redirect:/login";
+            if (isAjaxRequest) {
+                response.put("success", false);
+                response.put("error", "You must be logged in to change your password");
+                return ResponseEntity.ok(response);
+            } else {
+                redirectAttributes.addFlashAttribute("error", "You must be logged in to change your password");
+                return "redirect:/login";
+            }
         }
 
         // Validate current password
         if (!user.isMatchingPassword(currentPassword)) {
             logger.warn("Current password validation failed for user: {}", user.getUsername());
-            model.addAttribute("error", "Current password is incorrect");
-            model.addAttribute("title", "Change Password");
-            return view("account/change-password", model);
+            if (isAjaxRequest) {
+                response.put("success", false);
+                response.put("error", "Current password is incorrect");
+                return ResponseEntity.ok(response);
+            } else {
+                model.addAttribute("error", "Current password is incorrect");
+                model.addAttribute("title", "Change Password");
+                return view("account/change-password", model);
+            }
         }
 
         // Validate new password and confirmation match
         if (!newPassword.equals(confirmPassword)) {
             logger.warn("New password and confirmation do not match for user: {}", user.getUsername());
-            model.addAttribute("error", "New password and confirmation do not match");
-            model.addAttribute("title", "Change Password");
-            return view("account/change-password", model);
+            if (isAjaxRequest) {
+                response.put("success", false);
+                response.put("error", "New password and confirmation do not match");
+                return ResponseEntity.ok(response);
+            } else {
+                model.addAttribute("error", "New password and confirmation do not match");
+                model.addAttribute("title", "Change Password");
+                return view("account/change-password", model);
+            }
         }
 
         // Validate new password is not empty and meets minimum requirements
         if (newPassword.length() < 5) {
             logger.warn("New password does not meet minimum length requirements");
-            model.addAttribute("error", "New password must be at least 5 characters long");
-            model.addAttribute("title", "Change Password");
-            return view("account/change-password", model);
+            if (isAjaxRequest) {
+                response.put("success", false);
+                response.put("error", "New password must be at least 5 characters long");
+                return ResponseEntity.ok(response);
+            } else {
+                model.addAttribute("error", "New password must be at least 5 characters long");
+                model.addAttribute("title", "Change Password");
+                return view("account/change-password", model);
+            }
         }
 
         // Update the password
@@ -85,13 +117,25 @@ public class AccountController extends BaseController {
             userRepository.save(user);
 
             logger.info("Password successfully changed for user: {}", user.getUsername());
-            redirectAttributes.addFlashAttribute("success", "Your password has been successfully updated");
-            return "redirect:/account/change-password";
+            if (isAjaxRequest) {
+                response.put("success", true);
+                response.put("message", "Your password has been successfully updated");
+                return ResponseEntity.ok(response);
+            } else {
+                redirectAttributes.addFlashAttribute("success", "Your password has been successfully updated");
+                return "redirect:/account/change-password";
+            }
         } catch (Exception e) {
             logger.error("Error changing password: {}", e.getMessage(), e);
-            model.addAttribute("error", "An error occurred while changing your password. Please try again.");
-            model.addAttribute("title", "Change Password");
-            return view("account/change-password", model);
+            if (isAjaxRequest) {
+                response.put("success", false);
+                response.put("error", "An error occurred while changing your password. Please try again.");
+                return ResponseEntity.ok(response);
+            } else {
+                model.addAttribute("error", "An error occurred while changing your password. Please try again.");
+                model.addAttribute("title", "Change Password");
+                return view("account/change-password", model);
+            }
         }
     }
 }
